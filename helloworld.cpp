48edfe4444dc0355f1f951bcf3411ec918510902
LR44444 <168348788+LR44444@users.noreply.github.com>
2024-04-28 16:26:36 +0300

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n;
+    std::cout << "Введите высоту треугольника: ";
+    std::cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            std::cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
